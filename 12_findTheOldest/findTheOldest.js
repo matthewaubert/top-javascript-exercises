348865a95ae0107a7c2eaabb9d84b5ebe10434b0
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-26 16:48:20 -0400

    Solve problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // reduce people to oldest
+  return people.reduce((oldestPerson, currentPerson) => {
+    const opAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const cpAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+    // if current person age is more than oldest person age, set oldestPerson to currentPerson
+    return cpAge > opAge ? currentPerson : oldestPerson;
+  }); 
 };
+
+function getAge(birth, death) {
+  return death ? death - birth : (new Date().getFullYear()) - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
