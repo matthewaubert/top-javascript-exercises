9822a26ea2a2a6fc043cbe5e56eff7fd81568de1
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-26 15:52:10 -0400

    Solve problem 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // special chars
+  const special = ".,:;?! ";
+  // iterate through chars in string from beginning and end
+  for (let i = 0, j = string.length - 1; i < j; i++, j--) {
+    // while char is special char, skip
+    while (special.includes(string[i])) i++;
+    while (special.includes(string[j])) j--;
+    // if char i is not same as char j, return false
+    if (string[i].toLowerCase() !== string[j].toLowerCase()) return false;
+  }
+  return true; // if i < j return true
 };
 
 // Do not edit below this line
